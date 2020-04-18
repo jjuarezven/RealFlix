b529eeada10d7a3f8ef6e8e5ef989eb6f814acc0
JOSE-LAPTOP\Jose <josejuarez78@gmail.com>
2020-04-18 13:31:06 -0500

    Adding show list component

--- RealFlix/ClientApp/src/app/app-routing.module.ts
+++ RealFlix/ClientApp/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ShowsListComponent } from './shows-list/shows-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: ShowsListComponent, pathMatch: 'full' },
+/* { path: 'blogpost/:id', component: BlogPostComponent },
+{ path: 'add', component: BlogPostAddEditComponent },
+{ path: 'blogpost/edit/:id', component: BlogPostAddEditComponent }, */
+{ path: '**', redirectTo: '/' }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

1861d0e5be5bbbba2035d9cc8420fed825bd9839
JOSE-LAPTOP\Jose <josejuarez78@gmail.com>
2020-04-18 08:43:14 -0500

    Add project files.

---
+++ RealFlix/Models/Schedule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RealFlix.Models
+{
+    public class Schedule
+    {
+        public DateTime Time { get; set; }
+        public Days Days { get; set; }
+    }
+
+    public enum Days
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+}
---
+++ RealFlix/Models/Show.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RealFlix.Models
+{
+    public class Show
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Url { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Type { get; set; }
+        [Required]
+        public string Language { get; set; }
+        [Required]
+        public Genre Genres { get; set; }
+        [Required]
+        public string Status { get; set; }
+        [Required]
+        public int Runtime { get; set; }
+        [Required]
+        public DateTime Premiered { get; set; }
+        public string OfficialSite { get; set; }
+        public Schedule Schedule { get; set; }
+        [Required]
+        public string WebChannel { get; set; }
+        [Required]
+        public string Image { get; set; }
+        [Required]
+        public string Summary { get; set; }
+    }
+}

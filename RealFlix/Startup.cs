56336bb1bbfdbde591f06d343a27e52d724ddec8
JOSE-LAPTOP\Jose <josejuarez78@gmail.com>
2020-04-18 12:51:24 -0500

    Bootstrapping angular

--- RealFlix/Startup.cs
+++ RealFlix/Startup.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
 using RealFlix.Data;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
+using System.IO;
 
 namespace RealFlix
 {
@@ -48,6 +44,11 @@ namespace RealFlix
             });
 
             services.AddScoped(typeof(IDataRepository<>), typeof(DataRepository<>));
+
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/dist";
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -71,6 +72,19 @@ namespace RealFlix
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSpa(spa =>
+            {
+                // To learn more about options for serving an Angular SPA from ASP.NET Core,
+                // see https://go.microsoft.com/fwlink/?linkid=864501
+                
+                spa.Options.SourcePath = "ClientApp";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseAngularCliServer(npmScript: "start");
+                }
             });
         }
     }

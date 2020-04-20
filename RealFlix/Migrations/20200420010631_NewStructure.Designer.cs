ff5fd1f3a7c60da87251ba263c8adfb580a9a32a
JOSE-LAPTOP\Jose <josejuarez78@gmail.com>
2020-04-19 22:08:40 -0500

    Adding data

--- RealFlix/Migrations/20200420010631_NewStructure.Designer.cs
+++ RealFlix/Migrations/20200420010631_NewStructure.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace RealFlix.Migrations
 {
     [DbContext(typeof(RealFlixContext))]
-    partial class RealFlixContextModelSnapshot : ModelSnapshot
+    [Migration("20200420010631_NewStructure")]
+    partial class NewStructure
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- RealFlix/Models/Show.cs
+++ RealFlix/Models/Show.cs
@@ -21,16 +21,33 @@ namespace RealFlix.Models
         public string Genres { get; set; }
         [Required]
         public string Status { get; set; }
+        [Required]
+        public int Runtime { get; set; }
+        public DateTime Premiered { get; set; }
         public string OfficialSite { get; set; }
-        public string WebChannel { get; set; }
-        [Required]
-        public string Image { get; set; }
-        [Required]
-        public string Summary { get; set; }
         [Required]
         public string ScheduledTime { get; set; }
         [Required]
         public string ScheduledDays { get; set; }
+        public decimal RatingAverage { get; set; }
+        public int Weight { get; set; }
+        public int NetworkId { get; set; }
+        public string NetworkName { get; set; }
+        public string NetworkCountryName { get; set; }
+        public string NetworkCountryCode { get; set; }
+        public string WebChannel { get; set; }
+        public decimal ExternalsTvrage { get; set; }
+        public decimal ExternalsThetvdb { get; set; }
+        public decimal ExternalsImdb { get; set; }
+        public string ImageMedium { get; set; }
+        [Required]
+        public string ImageOriginal { get; set; }
+        [Required]
+        public string Summary { get; set; }
+        public DateTime Updated { get; set; }
+        public string LinksSelfHref { get; set; }
+        public string LinksPreviousEpisodeHref { get; set; }
+        public string LinksNextEpisodeHref { get; set; }
         public string Keywords { get; set; }
 
     }
